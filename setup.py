1994df8e24199280b6ee4fa45cae45380767de6d
Neerajonly <neerajgupta83699email@example.com>
2025-08-30 11:32:16 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is top 3 itmes in swiggy',
+    author='neeraj gupta',
+    license='',
+)
